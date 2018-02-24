3de1fc8d148d6f20fc95d6cfbd19b3b415803c2e
benweizhu <benweizhu@gmail.com>
2018-02-24 16:09:18 +0800

    enable react

--- side-nav/index.js
+++ side-nav/index.js
@@ -1,1 +1,8 @@
-console.log('abc')
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Hello React</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
