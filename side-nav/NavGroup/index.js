5efbb3ab8f99958ddfb76b8a081acfc37e7e3ae6
benweizhu <benweizhu@gmail.com>
2018-02-24 17:30:40 +0800

    change NavGroup to React Component

--- side-nav/NavGroup/index.js
+++ side-nav/NavGroup/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./nav-group.scss";
 
-const NavGroup = props => {
-  return (
-    <li className="nav-group">
-      {props.title}
-      <ul className="nav-group-item">{props.children}</ul>
-    </li>
-  );
-};
+class NavGroup extends React.Component {
+  render() {
+    return (
+      <li className="nav-group">
+        {this.props.title}
+        <ul className="nav-group-item">{this.props.children}</ul>
+      </li>
+    );
+  }
+}
 
 export default NavGroup;
